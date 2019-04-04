803af10e0711b224f7358403f73288d32c9ea8e3
YJ <YJ1516268@outlook.com>
2019-04-04 10:41:53 +0800

    1

---
+++ C/small/counter.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void count();
+
+int main(void)
+{
+    int i = 0;
+    for (i = 0; i <= 5; i++) {
+        count();
+    }
+    return 0;
+}
+
+void count()
+{
+    /*声明一个静态局部变量*/
+    static int num = 0;
+    num++;
+    printf("%d\n", num);
+}
